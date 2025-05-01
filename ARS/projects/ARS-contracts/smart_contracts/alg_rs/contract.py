f459a49c2dd0f9e0d1d632d09bbb5fc54f01f188
Mohammad Danish <thatsdanish02@gmail.com>
2025-05-02 01:24:29 +0530

    Defined Global State Variables

--- ARS/projects/ARS-contracts/smart_contracts/alg_rs/contract.py
+++ ARS/projects/ARS-contracts/smart_contracts/alg_rs/contract.py
@@ -1,8 +1,9 @@
-from algopy import ARC4Contract, String
-from algopy.arc4 import abimethod
+# smart_contracts/alg_rs/contract.py
 
+from algopy import ARC4Contract, abi, GlobalStateValue, itxn, gtxn
+from algopy.arc4 import arc4
 
-class AlgRs(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+class ReputationContract(ARC4Contract):
+    # Global states
+    reputation_scores: GlobalStateValue[abi.Uint64]
+    soulbound_nft_id: GlobalStateValue[abi.Uint64]
