0f57344cc63bd2e775cffa371ef2ba54f1317bb7
Mohammad Danish <83268762+Danish0703@users.noreply.github.com>
2025-05-06 23:29:49 +0530

    set up contract

--- ARS/projects/ARS-contracts/tests/alg_rs_test.py
+++ ARS/projects/ARS-contracts/tests/alg_rs_test.py
@@ -5,3 +5,11 @@ from algopy import algod_client, get_private_key
 from algopy.deployment import deploy_contract
 from smart_contracts.alg_rs.contract import ReputationContract
 
+@pytest.fixture
+def setup_contract():
+    client = algod_client()
+    creator = get_private_key()
+    contract = ReputationContract()
+    app_id = deploy_contract(contract, client, creator)
+    return contract, client, creator, app_id
+
